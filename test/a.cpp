5eaea063664e898db6f23e773f859c9d5ea0e60c
ROCKTAKEY <rocktakey@gmail.com>
2019-12-14 15:39:03 +0900

    Add testcases.

--- test/a.cpp
+++ test/a.cpp
@@ -1,3 +1,13 @@
+#include <memory>
 #include "wSDL.hpp"
 
-int main(int, char*[]){ return 0; }
+int main(int, char*[]){
+    std::unique_ptr<wsdl::SDL> sdl = std::make_unique<wsdl::SDL>(wsdl::InitFlags::audio);
+    {
+        auto w = sdl->createWindow("aaa", 100, 100, 100, 100,
+                                  wsdl::WindowFlags::borderless);
+
+        auto r = w->createRenderer(wsdl::RendererFlags::software);
+    }
+    return 0;
+}
