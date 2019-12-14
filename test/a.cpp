ffb91da5f4312991cefc960dd08cbc2063c08839
ROCKTAKEY <rocktakey@gmail.com>
2019-12-14 16:49:43 +0900

    Refactoring.

--- test/a.cpp
+++ test/a.cpp
@@ -5,7 +5,7 @@ int main(int, char*[]){
     std::unique_ptr<wsdl::SDL> sdl = std::make_unique<wsdl::SDL>(wsdl::InitFlags::audio);
     {
         auto w = sdl->createWindow("aaa", 100, 100, 100, 100,
-                                  wsdl::WindowFlags::borderless);
+                                   wsdl::WindowFlags::borderless);
 
         auto r = w->createRenderer(wsdl::RendererFlags::software);
     }
